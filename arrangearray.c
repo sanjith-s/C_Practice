808e929eb45c71f6667934a77ef6019e1ba62daf
Maxx <rusher2max@gmail.com>
2020-12-17 10:56:51 +0530

    Program to arrange elements of an array

---
+++ arrangearray.c
@@ -0,0 +1,43 @@
+//Program to arrange elements of an array in ascending order :p
+#include<stdio.h>
+
+void main() {
+
+    int size;
+    printf("Enter the size of the array:");
+    scanf("%d",&size);
+
+    int arr[size],i;
+
+    for (i=0; i<size; i++) {
+        printf("ENTER THE elements :");
+        scanf("%d",&arr[i]);
+    }
+    int j,k,a,b;
+    for (j=0;j<size;j++) {
+        for (k=0;k<size;k++) {
+            if (arr[j] != arr[k]) {
+                if (arr[j] > arr[k]) {
+                    a = arr[j];
+                    b = arr[k];
+                    arr[j] = a;
+                    arr[k] = b;
+                }
+                else {
+                    a = arr[j];
+                    b = arr[k];
+                    arr[j] = b;
+                    arr[k] = a;
+                }
+            }     
+            else { 
+                continue;
+            }
+        }
+    }
+    int l;
+    printf("the elements of the array are :\n");
+    for (l=0;l<size;l++) {
+        printf("%d\n",arr[l]);
+    }
+}
